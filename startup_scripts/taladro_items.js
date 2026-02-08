8b079e143db77705cf2e8cbbaf4acc7ae4e55281
camaroncin2 <jdavid1201.md@gmail.com>
2026-02-07 18:17:12 -0600

    Add files via upload

--- startup_scripts/taladro_items.js
+++ startup_scripts/taladro_items.js
@@ -1,6 +1,7 @@
 StartupEvents.registry('item', event => {
   event.create('steel_coated_ingot').displayName('Lingote Revestido de Acero')
   event.create('incomplete_coated_ingot').displayName('Lingote Revestido Incompleto')
+  event.create('hot_raw_steel').displayName('Acero en Bruto Caliente')
   event.create('fragile_drill_head').displayName('Cabeza de Taladro Frágil')
   event.create('heated_drill_head').displayName('Cabeza de Taladro Caliente')
   event.create('tempered_drill_head').displayName('Cabeza de Taladro Templada')

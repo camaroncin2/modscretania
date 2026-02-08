b927d749355b69e421eedd1f506b6778e8ea4bdc
camaroncin2 <jdavid1201.md@gmail.com>
2026-02-07 20:08:28 -0600

    Add files via upload

--- server_scripts/taladro_recetas.js
+++ server_scripts/taladro_recetas.js
@@ -2,26 +2,39 @@ ServerEvents.recipes(event => {
 
   event.remove({ output: 'create:mechanical_drill' })
 
+  event.remove({ id: 'kubejs:hot_raw_steel_mixing' })
+  event.custom({
+    type: 'create:mixing',
+    ingredients: [
+      { item: 'kubejs:crude_iron' },
+      { item: 'create_ironworks:coal_dust' },
+      { fluid: 'minecraft:lava', amount: 1000 }
+    ],
+    results: [
+      { item: 'kubejs:hot_raw_steel', count: 1 }
+    ]
+  }).id('kubejs:hot_raw_steel_mixing')
+  
   event.remove({ id: 'kubejs:steel_coated_ingot_sequence' })
   event.custom({
     type: 'create:sequenced_assembly',
-    ingredient: { item: 'minecraft:iron_ingot' },
+    ingredient: { item: 'kubejs:hot_raw_steel' },
     loops: 1,
     results: [{ item: 'kubejs:steel_coated_ingot', count: 1 }],
     sequence: [
       {
         type: 'create:deploying',
-        ingredients: [{ item: 'kubejs:incomplete_coated_ingot' }, { item: 'create_ironworks:steel_sheet' }],
+        ingredients: [{ item: 'kubejs:incomplete_coated_ingot' }, { item: 'kubejs:forge_hammer_mk1' }],
         results: [{ item: 'kubejs:incomplete_coated_ingot' }]
       },
       {
         type: 'create:deploying',
-        ingredients: [{ item: 'kubejs:incomplete_coated_ingot' }, { item: 'create_ironworks:steel_sheet' }],
+        ingredients: [{ item: 'kubejs:incomplete_coated_ingot' }, { item: 'kubejs:forge_hammer_mk1' }],
         results: [{ item: 'kubejs:incomplete_coated_ingot' }]
       },
       {
         type: 'create:deploying',
-        ingredients: [{ item: 'kubejs:incomplete_coated_ingot' }, { item: 'create_ironworks:steel_sheet' }],
+        ingredients: [{ item: 'kubejs:incomplete_coated_ingot' }, { item: 'kubejs:forge_hammer_mk1' }],
         results: [{ item: 'kubejs:incomplete_coated_ingot' }]
       }
     ],
